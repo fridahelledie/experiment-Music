06f40981656037dfadd5bc0be2402256d1904b55
AJ <151622122+ajohan23@users.noreply.github.com>
2025-03-25 12:57:50 +0100

    Small adjustments and fixes to online dtw

--- Eksperimenter/online-dtw-v5.py
+++ Eksperimenter/online-dtw-v5.py
@@ -1,3 +1,5 @@
+from logging import exception
+
 import sounddevice as sd
 import librosa
 import numpy as np
@@ -6,11 +8,9 @@ from librosa.sequence import dtw
 import matplotlib.pyplot as plt
 import soundfile as sf
 
-P = [] #Warping path
+P = [(0, 0)] #Warping path
 
-#current warping path end point
-i = 0 # input
-j = 0 # reference
+c = 10
 
 #Reference audio
 reference_audio_path = "audio_file.wav"
@@ -18,6 +18,7 @@ y, sr = librosa.load(reference_audio_path, sr=None)
 Y_chroma = librosa.feature.chroma_stft(y=y, sr=sr)
 
 #Input audio
+input_audio_path = "audio_file_slowed.wav"
 audio_queue = queue.Queue()
 X_chroma = np.empty(shape=(Y_chroma.shape[0], 0))
 
@@ -36,7 +37,7 @@ def show_path(P):
     plt.plot(P[:, 1], P[:, 0], marker="o", linestyle="-", markersize=3)
     plt.xlabel("Reference Audio (j)")
     plt.ylabel("Live Audio (i)")
-    plt.title("Alignment Path")
+    plt.title("Alignment Path and cost matrix")
     plt.grid()
     plt.show()
 
@@ -45,7 +46,7 @@ def show_matrix(D):
     plt.imshow(D, cmap='hot')
     plt.xlabel("Reference Audio (j)")
     plt.ylabel("Live Audio (i)")
-    plt.title("Alignment Path")
+    plt.title("Cost matrix")
     plt.grid()
     plt.show()
 
@@ -55,7 +56,7 @@ def EvaluatePathCost(i, j, X, Y):
 def GetInc(i, j, X, Y):
     pass
 
-def simulated_input(audio_path, buffer_size, X_chroma):
+def simulated_input(audio_path, buffer_size, X_chroma, Y_chroma, i_start=0, j_start=0):
     input_file = sf.SoundFile(audio_path)
     x_chroma = X_chroma
 
@@ -75,39 +76,66 @@ def simulated_input(audio_path, buffer_size, X_chroma):
         audio_queue.put(audio_chunk)
 
         #Handle audio data
-        x_chroma = process_audio_queue(sr=input_file.samplerate, x_chroma=x_chroma)
+        i, j = P[-1] #current warping path point
+        print(f"i:{i}, j:{j}")
+        x_chroma, D = process_audio_queue(sr=input_file.samplerate, x_chroma=x_chroma, i=i, j=j)
 
+        #Calculate path
+        path_segment = calculate_path_segment(D, i, j)
+        for point in path_segment:
+            P.append((i + point[0], j + point[1]))
+
+        if len(np.array(path_segment).shape) == 2: #This if statement is to catch a bug that happens
+            show_dtw(D, np.array(path_segment))
+
+
+        # print(f"P : {P}")
+        # show_dtw(D=D, P=np.array(P))
+
+    #calculate and show final cost matrix
     D, wp = dtw(x_chroma, Y_chroma, metric='euclidean')
-    show_matrix(D)
+    show_dtw(D, np.array(P))
 
 
-def process_audio_queue(sr, x_chroma):
+def process_audio_queue(sr, x_chroma, i, j):
     if not audio_queue.empty():
         audio_chunk = audio_queue.get()
 
         #Compute chroma feature for audio chunk
         chroma = librosa.feature.chroma_stft(y=audio_chunk, sr=sr)
         new_x_chroma = np.append(x_chroma, chroma, axis=1)
+        # print(new_x_chroma.shape)
 
         #calculate dtw matrix from current position (i, j) to current max
-        # print(x_chroma.shape)
-        # print(Y_chroma.shape)
-        # D, wp = dtw(new_x_chroma, Y_chroma, metric='euclidean')
+        D, wp = dtw(new_x_chroma[:,i:], Y_chroma[:,j:min(j+c, Y_chroma.shape[1])], metric='euclidean')
         # show_matrix(D)
-        return new_x_chroma
+
+        return new_x_chroma, D
 
 #Calculates dtw path from current i, j position to
-def calculate_path_segment(D, i, j, I, J):
-    i_current = i
-    j_current = j
-    path_points = ()
-    while i_current != I and j_current != J:
-        possible_paths = {(i_current, j_current + 1): D[i_current, j_current + 1],
-                          (i_current, j_current): D[i_current + 1, j_current],
+def calculate_path_segment(D, i, j):
+    i_current = 0
+    j_current = 0
+    path_points = []
+    # print(D.shape)
+    while i_current < D.shape[0] - 1 and j_current < D.shape[1] - 1:
+        #add all possible steps to dictionary
+        possible_steps = {(i_current, j_current + 1): D[i_current, j_current + 1],
+                          (i_current + 1, j_current): D[i_current + 1, j_current],
                           (i_current + 1, j_current + 1): D[i_current + 1, j_current + 1]}
 
+        #choose the cheapest step
+        # print(possible_steps.items())
+        step, _ = min(possible_steps.items(), key=lambda item: item[1])
+        # print(step)
+        #Add step to path
+        path_points.append((step[0], step[1]))
 
+        #update current path possition
+        i_current = step[0]
+        j_current = step[1]
 
-    pass
+    # print (path_points)
+    return path_points
 
-simulated_input(reference_audio_path, buffer_size=1048, X_chroma=X_chroma)
+simulated_input(input_audio_path, buffer_size=4096, X_chroma=X_chroma, Y_chroma=Y_chroma)
