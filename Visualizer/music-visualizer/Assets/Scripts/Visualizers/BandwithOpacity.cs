8f2268a968df11d5adb03fab27e0b8e54bfbbd0b
ZeeEms <152156488+ZeeEms@users.noreply.github.com>
2025-04-28 08:46:35 +0200

    Bandwith added but has to be reworked
    skal lige ændre fra chroma value til og kigge på hvor mange chromer der er igang på samme tid og muligvis gøre så det bliver bump map der bliver edited i stedet for opacity

--- Visualizer/music-visualizer/Assets/Scripts/Visualizers/BandwithOpacity.cs
+++ Visualizer/music-visualizer/Assets/Scripts/Visualizers/BandwithOpacity.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BandwithOpacity : MonoBehaviour
+public class BandwithOpacity : FeatureVisualizer
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Material mat;
+    private Renderer rendr;
+
+    private float targetValue = 1f; // Chromatic strength driving opacity - Has to be changed to the more than 2 chromatic shit
+                                    
+    void Awake()
     {
-        
+        //Getting material
+        rendr = GetComponent<Renderer>();
+        mat = rendr.material; 
+        EnableTransparency();
     }
 
-    // Update is called once per frame
+    public override void UpdateFeature(float value)
+    {
+        targetValue = value; // has to be changed to if statement
+    }
+
+
+
     void Update()
     {
-        
+        if (mat != null)
+        {
+            Color color = mat.color;
+            color.a = Mathf.Lerp(0f, 1f, targetValue); // Fade alpha based on chroma value, has to be changed 
+            mat.color = color;
+        }
+    }
+
+    void EnableTransparency()
+    {
+        if (mat != null)
+        {
+            mat.SetFloat("_Mode", 3); // Set Standard Shader mode to Transparent
+            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = 3000;
+        }
     }
 }
